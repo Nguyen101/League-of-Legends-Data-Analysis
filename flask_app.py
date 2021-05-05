76d20ea2e9793fdf0031a22251dafc240e797d61
DanielStrub <dstrub@live.com>
2021-05-05 15:54:08 +0000

    updated flask file

--- flask_app.py
+++ flask_app.py
@@ -44,4 +44,5 @@ def predict():
         return "Error making prediction", 400
 
 if __name__ == "__main__":
-    app.run()
+    port = os.environ.get("PORT", 5000)
+    app.run(debug="False", host="0.0.0.0", port=80)
