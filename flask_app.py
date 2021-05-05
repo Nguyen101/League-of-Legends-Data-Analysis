66963541bf3aef81c385fc052edbcc722b501525
DanielStrub <dstrub@live.com>
2021-05-05 16:08:19 +0000

    fixed port in flaskapp

--- flask_app.py
+++ flask_app.py
@@ -45,4 +45,4 @@ def predict():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT", 5000)
-    app.run(debug="False", host="0.0.0.0", port=80)
+    app.run(debug="False", host="0.0.0.0", port=port)
